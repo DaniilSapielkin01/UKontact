9d69bc7ee053d0aa5bec73a8c9e262fdd7565cf9
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-02-18 14:08:07 +0200

    Updates

---
+++ precache-manifest.37ff52a98f987efd63e195d6669a5332.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf3cd94db104b0c6bf9550b97da671d",
+    "url": "/UKontact/index.html"
+  },
+  {
+    "revision": "bd7a5dc708f90593da8e",
+    "url": "/UKontact/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "2e511be9b46961b2b386",
+    "url": "/UKontact/static/css/main.b88038ab.chunk.css"
+  },
+  {
+    "revision": "bd7a5dc708f90593da8e",
+    "url": "/UKontact/static/js/2.92264776.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/UKontact/static/js/2.92264776.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e511be9b46961b2b386",
+    "url": "/UKontact/static/js/main.13ae3733.chunk.js"
+  },
+  {
+    "revision": "18822c3123a730f5f94a",
+    "url": "/UKontact/static/js/runtime-main.e7cbb252.js"
+  },
+  {
+    "revision": "055924cf3e9e1b1ed1ac8935b3d4a3e9",
+    "url": "/UKontact/static/media/fontello.055924cf.svg"
+  },
+  {
+    "revision": "5a82ab6b5025819c842792ebecd0fb64",
+    "url": "/UKontact/static/media/fontello.5a82ab6b.woff2"
+  },
+  {
+    "revision": "9db090068124c750b23553acc8644c2a",
+    "url": "/UKontact/static/media/fontello.9db09006.ttf"
+  },
+  {
+    "revision": "b0694e3d5af703e967ae1ea0685b3673",
+    "url": "/UKontact/static/media/fontello.b0694e3d.eot"
+  },
+  {
+    "revision": "b722eaac199ca11c666fa52c4d84084d",
+    "url": "/UKontact/static/media/fontello.b722eaac.woff"
+  },
+  {
+    "revision": "932094ecc81e00683f772a2c16388ce7",
+    "url": "/UKontact/static/media/user-avatar.932094ec.jpg"
+  }
+]);
